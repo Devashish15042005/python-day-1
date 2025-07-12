e12619f9cc4d5bc4249f5ed5281e2a71bd070571
Devashish15042005 <devashishpandey1504@gmail.com>
2025-07-12 12:17:47 +0530

    Day 1: calculator , greeting bot , umit convertor

---
+++ greeting.py
@@ -0,0 +1,4 @@
+name = input("enter your name?")
+age = input("eneter your age?")
+print(f"hello {name}, hope you are doing well") 
+print(f"you are {age}, years old")
---
+++ unit covertor.py
@@ -0,0 +1,3 @@
+cm = float(input("enter your heightin cm:"))
+inches = cm/2.54
+print(f"{cm} cm is {inches: .2f}inches.")
