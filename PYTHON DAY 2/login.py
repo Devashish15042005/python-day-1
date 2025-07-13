8205019fb3b772a7ec5d6a2a4b1726c5cc4d49e8
Devashish15042005 <devashishpandey1504@gmail.com>
2025-07-13 13:53:26 +0530

    completed day 2 tasks (if-else, loops, sum, table.

---
+++ PYTHON DAY 2/login.py
@@ -0,0 +1,6 @@
+username = input("enter your username")
+password = input("enter your password")
+if username == "dev" and password == 12345:
+ print("access granted")
+else:
+ print("access denied") 
